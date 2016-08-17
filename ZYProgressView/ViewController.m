6d1838b807bccea949661a073b27948ff23b20b0
Yanyinghenmei <1113135272@qq.com>
2016-08-17 18:37:55 +0800

    add some example

--- ZYProgressView/ViewController.m
+++ ZYProgressView/ViewController.m
@@ -15,6 +15,8 @@
 
 @implementation ViewController {
     ZYProgressView *pro;
+    ZYProgressView *pro1;
+    ZYProgressView *pro2;
 }
 
 - (void)viewDidLoad {
@@ -29,6 +31,8 @@
     proView.trackTintColor = [UIColor cyanColor];
     proView.tintColor = [UIColor orangeColor];
     
+    
+    
     pro = [[ZYProgressView alloc] initWithFrame:CGRectMake(10, 200, 300, 30)];
     [self.view addSubview:pro];
     pro.isSlider = YES;
@@ -36,13 +40,25 @@
         NSLog(@"%f", progress);
     };
     
+    //    pro.progressType = 1;
+    //    pro.backColor = [UIColor lightGrayColor];
+    //    pro.progressColors = @[(id)[UIColor yellowColor].CGColor, (id)[UIColor blueColor].CGColor];
     
-//    pro.progressType = 1;
-//    pro.backColor = [UIColor lightGrayColor];
-//    pro.progressColors = @[(id)[UIColor yellowColor].CGColor, (id)[UIColor blueColor].CGColor];
     
     
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(150, CGRectGetMaxY(pro.frame)+100, 80, 50)];
+    pro1 = [[ZYProgressView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(pro.frame)+ 30, 300, 30)];
+    [self.view addSubview:pro1];
+    pro1.progress = 0.76;
+    
+    
+    pro2 = [[ZYProgressView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(pro1.frame)+ 30, 300, 30)];
+    [self.view addSubview:pro2];
+    pro2.progressType = ProgressTypeMaskImage;
+    pro2.progress = 0.52;
+    
+    
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(150, CGRectGetMaxY(pro2.frame)+100, 80, 50)];
     [self.view addSubview:btn];
     btn.backgroundColor = [UIColor orangeColor];
     [btn setTitle:@"click" forState:UIControlStateNormal];
@@ -51,6 +67,8 @@
 
 - (void)btnClick:(UIButton *)btn {
     pro.progress = arc4random()%100/100.00;
+    pro1.progress = arc4random()%100/100.00;
+    pro2.progress = arc4random()%100/100.00;
 }
 
 - (void)didReceiveMemoryWarning {
